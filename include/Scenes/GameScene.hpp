67b9f430984821ed0059c3d2c63b59974fab49d1
AlexMainstone <alecsmainstone@gmail.com>
2021-03-16 00:00:54 +0000

    Initial Commit

---
+++ include/Scenes/GameScene.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <entt/entt.hpp>
+
+class GameScene {
+    public:
+        GameScene(sf::RenderWindow &window);
+        
+        void handleEvent(sf::Event e);
+        void update(float dt);
+        void render();
+    private:
+        sf::RenderWindow &window;
+};
---
+++ src/Main.cpp
@@ -0,0 +1,37 @@
+#include <SFML/Graphics.hpp>
+#include "Scenes/GameScene.hpp"
+
+void main() {
+    //TODO load window settings
+    
+    // Create Window
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "cpp-engine");
+    
+    // TODO abstract scene class
+    GameScene *scene = new GameScene(window);
+    
+    // Clock that counts time between updates
+    sf::Clock delta_clock;
+
+    while(window.isOpen()) {
+        // Handle Events
+        sf::Event e;
+        while(window.pollEvent(e)) {
+            // Check if window closed
+            if(e.type == sf::Event::Closed) {
+                window.close();
+            }
+            
+            // Pass Event to scene
+            scene->handleEvent(e);
+        }
+        
+        // Update & restart clock
+        scene->update(delta_clock.restart().asSeconds());
+        
+        // Render
+        window.clear();
+        scene->render();
+        window.display();
+    }
+}
